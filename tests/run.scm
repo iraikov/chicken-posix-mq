92bb0e888867959cdcf2eb5c4d12dbaff5c85327
Nevroz Arslan <newrozarslan@gmail.com>
2022-06-05 02:43:13 +0300

    write procedures which represent test cases

--- tests/run.scm
+++ tests/run.scm
@@ -1,4 +1,4 @@
-(import scheme (chicken format) (chicken random) (chicken file posix) (chicken string) (chicken blob) posix-mq)
+(import scheme (chicken format) (chicken random) (chicken file posix) (chicken string) (chicken blob) test posix-mq)
 
 
 (print posix-mq?)
@@ -14,3 +14,41 @@
   )
 
 
+(define msg "Hello, world!")
+(define encoded-msg (string->blob msg))
+(define path (sprintf "/mqtest~A" (pseudo-random-integer 100000000)))
+
+(define create-path
+  (lambda () (mq-create path  10 1000 oflags: (list open/rdwr))))
+(define info-path
+  (lambda () (mq-info path)))
+(define send-path
+  (lambda () (mq-send path encoded-msg)))
+(define recv-path
+  (lambda () (blob->string(mq-recv path encoded-msg))))
+(define delete-path
+  (lambda () (mq-unlink path)))
+
+(define test-mq-rcv
+  (lambda () (
+	      (let (
+		    (fd (create-path))
+		    (sd (send-path))
+		    (id  (info-path))
+		    (decoded-msg (recv-path))
+		    (dd (delete-path))
+		    )
+		(sprintf "~A" decoded-msg)
+		)
+	      
+	      ))
+
+  )
+
+(test-group "mq-recv"
+  (test "received message should be equal to msg"
+	msg
+	(test-mq-rcv) )
+  )
+
+(test-exit)
