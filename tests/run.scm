cf86f4f9630071095b691a9e5cf04bcbf5837432
Nevroz Arslan <newrozarslan@gmail.com>
2022-06-14 03:49:24 +0300

    simplify tests

--- tests/run.scm
+++ tests/run.scm
@@ -1,68 +1,57 @@
 (import scheme (chicken format) (chicken random) (chicken file posix) (chicken string) (chicken blob) test posix-mq)
 
-; 
-; (print posix-mq?)
-; (let* ((str "Hello, world!")
-      ;  (path (sprintf "/mqtest~A" (pseudo-random-integer 100)))
-      ;  (fd (mq-create path  10 1000 oflags: (list open/rdwr)))
-      ;  (msg (string->blob str)))
-    ; (print (mq-info path)) ;; -> this makes test fail
-    ; (print "sending " msg)
-    ; (mq-send path msg)
-    ; (print "received " (mq-recv path))
-    ; (mq-unlink path)
-  ; )
-; 
+
+(print posix-mq?)
+(let* ((str "Hello, world!")
+			 (path (sprintf "/mqtest~A" (pseudo-random-integer 100)))
+			 (fd (mq-create path  10 1000 oflags: (list open/rdwr)))
+			 (msg (string->blob str)))
+	(print (mq-info path)) 
+	(print "sending " msg)
+	(mq-send path msg)
+	(print "received " (mq-recv path))
+	(mq-unlink path))
+ 
 
 (define msg "Hello, world!")
 (define encoded-msg (string->blob msg))
-(define *default-path* (sprintf "/mqtest~A" (pseudo-random-integer 100000000)))
+(define *default-path* (sprintf "/mqtest~A" (pseudo-random-integer 100)))
 
-(define create-path
-  (lambda (path) (mq-create path  10 1000 oflags: (list open/rdwr))))
-(define info-path
+(define (create-queue path)
+  (mq-create path  10 1000 oflags: (list open/rdwr)))
+(define info-queue
   (lambda (path) (mq-info path)))
-(define send-path
+(define send-to-queue
   (lambda (path) (mq-send path encoded-msg)))
-(define recv-path
+(define recv-from-queue
   (lambda (path) (blob->string(mq-recv path encoded-msg))))
-(define delete-path
+(define delete-queue
   (lambda (path) (mq-unlink path)))
 
-(define test-mq-recv?
-  (lambda (path) (
-	      (let (
-		    (fd (create-path path))
-		    (sd (send-path path))
-		    (id (info-path path))
-		    (decoded-msg (recv-path path))
-		    (dd (delete-path path))
-		    )
-		decoded-msg
-		))))
+(define (mq-recv-aux path)
+  (let ((fd (create-queue path))
+				(sd (send-to-queue path))
+				(id (info-queue path))
+				(decoded-msg (recv-from-queue path))
+				(dd (delete-queue path)))
+		decoded-msg))
 
 
-(define test-mq-info?
-  (lambda (path) (
-	      (let* (
-		    (fd [create-path path] )
-		    (sd [send-path path])
-        (id [info-path path]) 
-        )
-        (length id))
-		    )
-      id
-		))
+(define (mq-info-aux path)
+ (let ((fd [create-queue path])
+					 (sd [send-to-queue path])
+					 (id [info-queue path]))
+			 (length id)))
 
 
 (test-group "receive"
-  (test-assert "received message should be equal to sent msg"
-	(test-mq-recv? *default-path*) )
-  )
+  (test "received message should be equal to sent msg"
+				msg
+				(mq-recv-aux *default-path*)))
 
 (test-group "info"
-  (test-assert "holds three pairs"
-	 (test-mq-info? *default-path*)  ) )
+  (test "queue holds three pairs" 3
+	 (mq-info-aux *default-path*)))
 
 
 (test-exit)
